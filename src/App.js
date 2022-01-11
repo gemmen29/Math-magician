d7b9406b68bc8ef3ec8596b17c2e0177e3b9871c
George Magdy <georgtriple@gmail.com>
2022-01-11 12:51:14 +0200

    Implement routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
 import Calculator from './components/Calculator/Calculator';
+import Quote from './components/Quote/Quote';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Calculator />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/home" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quote />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
     </div>
   );
 }
